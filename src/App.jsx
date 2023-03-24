7fe91d7759d648d1f02916b289c11fc0dd1eaa32
Aelson Martins <aelsonmartins1302@gmail.com>
2023-03-24 06:36:47 -0400

    header and first section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,39 @@
-export default function App() {
+import styles from './App.module.css';
 
+export default function App() {
   return (
     <div className="App">
-      <h1>Conteudo do Site</h1>
-    </div>
-  )
-}
+      <span></span>
+      <div className={styles.shaderTopBlack}></div>
+      <div className={styles.img_fixed}></div>
 
+      {/* <section>
+        <h1 className={styles.title}>Modelos Disponíveis</h1>
+        <div className={styles.slider}>
+          <img id="slider_1" src="../src/img/audi_a3.png" alt="img_carousel" />
+
+          <img id="slider_2" src="../src/img/audi_a4.png" alt="img_carousel" />
+
+          <img id="slider_3" src="../src/img/audi_a5.png" alt="img_carousel" />
+
+          <img id="slider_4" src="../src/img/audi_a6.png" alt="img_carousel" />
+
+          <img
+            id="slider_5"
+            src="../src/img/audi_e_tron.png"
+            alt="img_carousel"
+          />
+
+          <img
+            id="slider_6"
+            src="../src/img/audi_e_tron_gt.png"
+            alt="img_carousel"
+          />
+        </div>
+      </section> */}
+      <br />
+      <br />
+      <br />
+    </div>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import Header from './assets/components/header/Header.jsx'
-import Footer from './assets/components/footer/Footer.jsx'
-import '../src/global.module.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import Header from './assets/components/header/Header.jsx';
+import Footer from './assets/components/footer/Footer.jsx';
+import '../src/global.module.css';
+import '../src/script.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -11,4 +12,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
     <Footer />
   </React.StrictMode>,
-)
+);
